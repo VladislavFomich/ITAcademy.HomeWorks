f067b4a86c506807590ef2f905827488d46f8cf5
Lonzelot <vladfomich123@gmail.com>
2020-11-06 23:39:20 +0300

    Задача 5

--- HW04.Task2/HW04.Task2/Program.cs
+++ HW04.Task2/HW04.Task2/Program.cs
@@ -10,9 +10,10 @@ namespace HW04.Task2
             Calculator();
             MathCheck();
             MathCheck2();
+            Task5();
         }
 
-        static void Calculator()
+       static void Calculator()
         {
             Console.WriteLine("Введите первое число");
             int num1 = Convert.ToInt32(Console.ReadLine());
@@ -48,6 +49,7 @@ namespace HW04.Task2
 
           
             }
+
         static void MathCheck2()
         {
             Console.WriteLine("Введите первое число");
@@ -79,5 +81,51 @@ namespace HW04.Task2
             }
 
         }
+
+        static void Task5()
+        {
+            Console.WriteLine("Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор + или -");
+            char inputOp = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите результат операции");
+            int inputSum = Convert.ToInt32(Console.ReadLine());
+
+            int realSum;
+
+            if (inputOp == '+')
+            {
+                realSum = num1 + num2;
+            }
+           else
+            {
+                realSum = num1 - num2;
+            }
+           
+                         
+            if (inputSum == realSum)
+            {
+                Console.WriteLine("Правильно");
+            }
+            else if (inputSum > realSum)
+            {
+                Console.WriteLine("Должно быть меньше");
+            }
+            else if (inputSum < realSum)
+            {
+                Console.WriteLine("Должно быть больше");
+            }
+            else
+            {
+                Console.WriteLine("Неправильно");
+            }
+
+        }
     }
+   
 }

a4e8fa14d94ca8f402dd8f86d86bf249ead155c5
Lonzelot <vladfomich123@gmail.com>
2020-11-06 23:08:53 +0300

    Задание 4

--- HW04.Task2/HW04.Task2/Program.cs
+++ HW04.Task2/HW04.Task2/Program.cs
@@ -9,6 +9,7 @@ namespace HW04.Task2
         {
             Calculator();
             MathCheck();
+            MathCheck2();
         }
 
         static void Calculator()
@@ -39,6 +40,38 @@ namespace HW04.Task2
             if (inputSum == realSum)
             {
                 Console.WriteLine("Правильно");
+            }
+            else
+            {
+                Console.WriteLine("Неправильно");
+            }
+
+          
+            }
+        static void MathCheck2()
+        {
+            Console.WriteLine("Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите сумму чисел");
+            int inputSum = Convert.ToInt32(Console.ReadLine());
+
+            int realSum = num1 + num2;
+
+            if (inputSum == realSum)
+            {
+                Console.WriteLine("Правильно");
+            }
+            else if (inputSum > realSum)
+            {
+                Console.WriteLine("Должно быть меньше");
+            }
+            else if (inputSum < realSum)
+            {
+                Console.WriteLine("Должно быть больше");
             }
             else
             {

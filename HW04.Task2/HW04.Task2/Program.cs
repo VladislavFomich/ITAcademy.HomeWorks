be6ad018f65eb14b77b737c801d5ed6fa7fb4df1
Lonzelot <vladfomich123@gmail.com>
2020-11-06 22:35:39 +0300

    Задание 3

--- HW04.Task2/HW04.Task2/Program.cs
+++ HW04.Task2/HW04.Task2/Program.cs
@@ -8,6 +8,7 @@ namespace HW04.Task2
         static void Main(string[] args)
         {
             Calculator();
+            MathCheck();
         }
 
         static void Calculator()
@@ -20,6 +21,30 @@ namespace HW04.Task2
 
             int sum = num1 + num2;
             Console.WriteLine($"Суммв введенных чисел = {sum}");
+        }
+
+        static void MathCheck()
+        {
+            Console.WriteLine("Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите сумму чисел");
+            int inputSum = Convert.ToInt32(Console.ReadLine());
+
+            int realSum = num1 + num2;
+
+            if (inputSum == realSum)
+            {
+                Console.WriteLine("Правильно");
+            }
+            else
+            {
+                Console.WriteLine("Неправильно");
+            }
+
         }
     }
 }

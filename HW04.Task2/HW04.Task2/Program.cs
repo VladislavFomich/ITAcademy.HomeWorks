3bcf3dd644469fe3513028752ef493f8b5a73690
Lonzelot <vladfomich123@gmail.com>
2020-11-06 20:27:04 +0300

    Задача 2

---
+++ HW04.Task2/HW04.Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HW04.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator();
+        }
+
+        static void Calculator()
+        {
+            Console.WriteLine("Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.WriteLine($"Суммв введенных чисел = {sum}");
+        }
+    }
+}

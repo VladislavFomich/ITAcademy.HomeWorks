a02fda244da631ddc82cd191922ed15ee47c3fc5
Lonzelot <vladfomich123@gmail.com>
2020-11-07 12:44:52 +0300

    Исправлен алгоритм в задачах 4,5.  Изменил названия методов на более понятные.  Сделал более логичные отступы.

--- HW04.Task2/HW04.Task2/Program.cs
+++ HW04.Task2/HW04.Task2/Program.cs
@@ -7,80 +7,89 @@ namespace HW04.Task2
     {
         static void Main(string[] args)
         {
-            Calculator();
-            MathCheck();
-            MathCheck2();
+            Task2();
+            Task3();
+            Task4();
             Task5();
         }
 
-       static void Calculator()
-        {
-            Console.WriteLine("Введите первое число");
-            int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите второе число");
-            int num2 = Convert.ToInt32(Console.ReadLine());
 
-            int sum = num1 + num2;
-            Console.WriteLine($"Суммв введенных чисел = {sum}");
-        }
+          static void Task2()
+          { 
+               Console.WriteLine("Введите первое число");
+               int num1 = Convert.ToInt32(Console.ReadLine());
 
-        static void MathCheck()
-        {
-            Console.WriteLine("Введите первое число");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("Введите второе число");
+               int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите второе число");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+               int sum = num1 + num2;
+               Console.WriteLine($"Суммв введенных чисел = {sum}");
+          }
 
-            Console.WriteLine("Введите сумму чисел");
-            int inputSum = Convert.ToInt32(Console.ReadLine());
 
-            int realSum = num1 + num2;
 
-            if (inputSum == realSum)
-            {
-                Console.WriteLine("Правильно");
-            }
-            else
-            {
-                Console.WriteLine("Неправильно");
-            }
+           static void Task3()
+           {
+               Console.WriteLine("Введите первое число");
+               int num1 = Convert.ToInt32(Console.ReadLine());
 
-          
-            }
+               Console.WriteLine("Введите второе число");
+               int num2 = Convert.ToInt32(Console.ReadLine());
 
-        static void MathCheck2()
-        {
-            Console.WriteLine("Введите первое число");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("Введите сумму чисел");
+               int inputSum = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите второе число");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+               int realSum = num1 + num2;
 
-            Console.WriteLine("Введите сумму чисел");
-            int inputSum = Convert.ToInt32(Console.ReadLine());
+               if (inputSum == realSum)
+               {
+                   Console.WriteLine("Правильно");
+               }
+               else
+               {
+                   Console.WriteLine("Неправильно");
+               }
 
-            int realSum = num1 + num2;
 
-            if (inputSum == realSum)
-            {
-                Console.WriteLine("Правильно");
-            }
-            else if (inputSum > realSum)
-            {
-                Console.WriteLine("Должно быть меньше");
-            }
-            else if (inputSum < realSum)
-            {
-                Console.WriteLine("Должно быть больше");
-            }
-            else
-            {
-                Console.WriteLine("Неправильно");
-            }
+           }
 
-        }
+
+
+           static void Task4()
+           {
+               Console.WriteLine("Введите первое число");
+               int num1 = Convert.ToInt32(Console.ReadLine());
+
+               Console.WriteLine("Введите второе число");
+               int num2 = Convert.ToInt32(Console.ReadLine());
+
+               Console.WriteLine("Введите сумму чисел");
+               int inputSum = Convert.ToInt32(Console.ReadLine());
+
+               int realSum = num1 + num2;
+
+               if (inputSum == realSum)
+               {
+                   Console.WriteLine("Правильно");
+               }
+
+               else
+               {
+                   Console.WriteLine("Неправильно");
+               }
+
+               if (inputSum > realSum)
+               {
+                   Console.WriteLine("Должно быть меньше");
+               }
+               else if (inputSum < realSum)
+               {
+                   Console.WriteLine("Должно быть больше");
+               }
+           }
+
+
 
         static void Task5()
         {
@@ -93,39 +102,43 @@ namespace HW04.Task2
             Console.WriteLine("Введите оператор + или -");
             char inputOp = Convert.ToChar(Console.ReadLine());
 
-            Console.WriteLine("Введите результат операции");
-            int inputSum = Convert.ToInt32(Console.ReadLine());
-
             int realSum;
 
             if (inputOp == '+')
             {
                 realSum = num1 + num2;
             }
-           else
+            else if (inputOp == '-')
             {
                 realSum = num1 - num2;
             }
-           
-                         
+            else
+            {
+                Console.WriteLine("Вы ввелм неверный оператор");
+                realSum = 0;
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Введите результат операции");
+            int inputSum = Convert.ToInt32(Console.ReadLine());
+                 
             if (inputSum == realSum)
             {
                 Console.WriteLine("Правильно");
             }
-            else if (inputSum > realSum)
+            else
+            {
+                Console.WriteLine("Неправильно");
+            }
+            
+            if (inputSum > realSum)
             {
                 Console.WriteLine("Должно быть меньше");
             }
             else if (inputSum < realSum)
             {
                 Console.WriteLine("Должно быть больше");
-            }
-            else
-            {
-                Console.WriteLine("Неправильно");
-            }
-
+            }          
         }
-    }
-   
+    }  
 }

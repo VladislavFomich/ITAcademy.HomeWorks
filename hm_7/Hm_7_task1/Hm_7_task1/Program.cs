a8290005cebc76e6d1be8bdde331de4a7d216ba2
Lonzelot <vladfomich123@gmail.com>
2020-11-19 17:32:50 +0300

    Created Task1

---
+++ hm_7/Hm_7_task1/Hm_7_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hm_7_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стихотворение, отделяя строки символом \";\"");
+            string str = Console.ReadLine();
+            str = str.Replace('O', 'A'); 
+            str = str.Replace('О', 'А');
+            str = str.Replace('o', 'a');
+            str = str.Replace('о', 'а');
+            string[] words = str.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in words)
+            {
+                Console.WriteLine($"{s}");
+            }
+        }
+    }
+}

d1ff7ba67cd5752789f04de837f08c1befc05f1f
Lonzelot <vladfomich123@gmail.com>
2020-11-20 20:01:40 +0300

    Deleted Spaces

--- hm_06_task3/hm_06_task3/Program.cs
+++ hm_06_task3/hm_06_task3/Program.cs
@@ -10,27 +10,24 @@ namespace hm_06_task3
         {
             Stopwatch stopwatch = new Stopwatch();
             Stopwatch stopwatch1 = new Stopwatch();
-
             Console.WriteLine("Ваш массив:");
             int[] myArray = new int[1000000];
             for(int i = 0; i < myArray.Length; i++)
             {
                 Console.WriteLine(myArray[i]);
             }
-
             stopwatch.Start();
             Console.WriteLine("Реверс массива(Свой)");
+
             for (int i = myArray.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(myArray[i]);
             }
             stopwatch.Stop();
-            
-
-            
             Console.WriteLine("Снова реверс(Метод Reverse)");
             stopwatch1.Start();
             myArray.Reverse();
+
             for (int i = 0; i < myArray.Length; i++)
             {
                 Console.WriteLine(myArray[i]);
@@ -40,7 +37,6 @@ namespace hm_06_task3
             Console.WriteLine(stopwatch.ElapsedMilliseconds);
             Console.WriteLine("Затрачено миллисекунд импользуя метод");
             Console.WriteLine(stopwatch1.ElapsedMilliseconds);
-
         }
     }
 }

0d38d8c3ec87a92c188dfc5af207645eaf81edfc
Lonzelot <vladfomich123@gmail.com>
2020-11-23 14:44:45 +0300

    Created hm_08_task1

---
+++ hm_08/hm_8_task1/hm_8_task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace hm_8_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("1a!2.3!!..4.!.?6 7! ..?");
+
+            for (int i = 16; i <= '?'; i++)
+                sb.Replace(' ', '_');
+
+            sb.Remove(2, 1);
+            sb.Remove(5, 2);
+            sb.Remove(9, 1);
+            Console.WriteLine(sb);
+        }
+    }
+}

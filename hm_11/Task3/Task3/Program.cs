8cc200bd3d854fa0964efcb00ae963130b6eb0a0
Lonzelot <vladfomich123@gmail.com>
2020-12-08 23:42:15 +0300

    Task3

---
+++ hm_11/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Films film = new Films();
+            film.name = "Pirates of Carribean";
+            film.category = "Action";
+            film.size = 250;
+            film.producer = "Jerry Bruckhaimer";
+            film.mainActor = "Johny Depp";
+            film.mainActress = "Kira Naitli";
+            film.Play();
+            film.RetrieveInformation();
+
+            Music music = new Music();
+            music.name = "Bad Guy";
+            music.category = "Pop";
+            music.size = 15;
+            music.performer = "Billie Eillish";
+            music.lenght = 300;
+            music.Play();
+            music.RetrieveInformation();
+        }
+    }
+}

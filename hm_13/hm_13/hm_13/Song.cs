b568fad850dcb0d8b47ed3d0b3cc414961e75ece
Lonzelot <vladfomich123@gmail.com>
2020-12-13 22:29:20 +0300

    hm_13..v0.2

---
+++ hm_13/hm_13/hm_13/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hm_13
+{
+    class Song
+    {
+        string name;
+        int lenght;
+        string composer;
+        int year;
+
+    }
+}

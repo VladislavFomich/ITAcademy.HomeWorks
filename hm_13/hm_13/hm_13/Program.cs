5051ea4619bdd4d95c2de50f54855ff4a0fd95fa
Lonzelot <vladfomich123@gmail.com>
2020-12-15 22:30:31 +0300

    Created task

--- hm_13/hm_13/hm_13/Program.cs
+++ hm_13/hm_13/hm_13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 
 namespace hm_13
 {
@@ -6,7 +7,44 @@ namespace hm_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Song song = new Song();
+            Console.WriteLine("Введите имя песни:");
+            song.name = Console.ReadLine();
+            Console.WriteLine("Введите длинну песни:");
+            song.Lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите год песни:");
+            song.Year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите автора песни:");
+            song.composer = Console.ReadLine();
+            Console.WriteLine("Выберите жанр: 1 - Rock, 2 - Funk, 3 - Jazz, 4 - Classical, 5 - Rap, 6 - Reggie.");
+            string num = Console.ReadLine();
+
+            switch (num)
+            {
+                case "1":
+                    song.genre = Song.Genres.Rock;
+                    break;
+                case "2":
+                    song.genre = Song.Genres.Funk;
+                    break;
+                case "3":
+                    song.genre = Song.Genres.Jazz;
+                    break;
+                case "4":
+                    song.genre = Song.Genres.Classical;
+                    break;
+                case "5":
+                    song.genre = Song.Genres.Rap;
+                    break;
+                case "6":
+                    song.genre = Song.Genres.Reggie;
+                    break;
+                default:
+                    Console.WriteLine("Введено неверное число, введите данные заново.");
+                    break;                    
+            }
+            song.GetSongData(song);
+            
         }
     }
 }
--- hm_13/hm_13/hm_13/Song.cs
+++ hm_13/hm_13/hm_13/Song.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace hm_13
 {
     class Song
     {
-        string name;
-        int lenght;
-        string composer;
-        int year;
+       public Genres genre;
+       public string name;   
+       private int lenght;
+        public int Lenght
+        {
+            get { return lenght; }
+            set
+            {
+                if(value >= 0 && value < 1000)
+                lenght = value;
+            }
+        }
+        public string composer;
+        private int year;
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value >= 0 && value < 2020)
+                    year = value;
+            }
+        }
 
-    }
+        public enum Genres
+        {
+            Rock,
+            Funk,
+            Jazz,
+            Classical,
+            Rap,
+            Reggie
+        }
+
+        public void GetSongData(Song song)
+        {
+            var song1 = new
+            {
+                name = song.name,
+                Lenght = song.Lenght,
+                Year = song.Year,
+                composer = song.composer,
+                genre = song.genre
+            };
+            Console.WriteLine($"Название песни: {song1.name}, автор песни: {song1.composer}, длинна песни (секунд): {song1.Lenght}, год песни: {song1.Year}, жанр песни: {song1.genre}.");
+            string json = JsonSerializer.Serialize(song1);
+            Console.WriteLine(json);
+        }
+        
+    }  
 }

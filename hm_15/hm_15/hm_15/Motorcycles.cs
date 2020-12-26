b346a43a27e40362f0b153c7dd9b632b9c33959f
Lonzelot <vladfomich123@gmail.com>
2020-12-26 23:57:38 +0300

    hm_15v.0.5

---
+++ hm_15/hm_15/hm_15/Motorcycles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hm_15
+{
+    interface IRepository<T> : IDisposable
+    {
+        public void CreateMotorcycle(T item) { }
+        public void GetMotorCycleByID(int id) { }
+        public void GetMotorcycles(string Name, string Model, int Year) { }       
+        public void UpdateMotorycle(T item) { }
+        public void DeleteMotorcycle(int id) { }
+    }
+    class Motorcycles 
+    {
+        public Guid id { get; set; }
+        public string Name { get; set;}
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometer { get; set; }
+       
+        
+    }
+}
---
+++ hm_15/hm_15/hm_15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hm_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Motorcycles> motorcycle = new List<Motorcycles>();
+            
+        }
+    }
+}

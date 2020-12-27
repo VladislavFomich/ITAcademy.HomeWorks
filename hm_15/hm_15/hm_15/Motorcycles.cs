c11895e406384d1ca55ec96aa2b46863bed913d7
Lonzelot <vladfomich123@gmail.com>
2020-12-27 21:51:50 +0300

    hm_15v0.6

--- hm_15/hm_15/hm_15/Motorcycles.cs
+++ hm_15/hm_15/hm_15/Motorcycles.cs
@@ -4,22 +4,45 @@ using System.Text;
 
 namespace hm_15
 {
-    interface IRepository<T> : IDisposable
+  public interface IRepository
     {
-        public void CreateMotorcycle(T item) { }
-        public void GetMotorCycleByID(int id) { }
-        public void GetMotorcycles(string Name, string Model, int Year) { }       
-        public void UpdateMotorycle(T item) { }
-        public void DeleteMotorcycle(int id) { }
+        void CreateMotorcycle();
+        void GetMotorCycleByID();
+        void GetMotorcycles();
+        void UpdateMotorycle();
+        void DeleteMotorcycle();
     }
-    class Motorcycles 
+
+    [Serializable]
+   public class Motorcycles : IRepository
     {
         public Guid id { get; set; }
         public string Name { get; set;}
         public string Model { get; set; }
         public int Year { get; set; }
         public int Odometer { get; set; }
-       
-        
+
+
+        public void CreateMotorcycle()
+        {
+            Console.WriteLine($"Создали мотоцикл") ;
+        }
+        public void GetMotorCycleByID()
+        {
+            Console.WriteLine($"Индитификационный номер {id}");
+        }
+        public void GetMotorcycles()
+        {
+            Console.WriteLine($"{Name}, {Model}, {Year}, {Odometer}");
+        }
+        public void UpdateMotorycle() 
+        {
+            Odometer = +5000;
+            Console.WriteLine($"Ездим. Значение одометра равно {Odometer}");
+        }
+        public void DeleteMotorcycle()
+        {
+            Console.WriteLine("Заканчиваем поездку");
+        }
     }
 }
--- hm_15/hm_15/hm_15/Program.cs
+++ hm_15/hm_15/hm_15/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
 namespace hm_15
 {
@@ -7,8 +8,21 @@ namespace hm_15
     {
         static void Main(string[] args)
         {
-            List<Motorcycles> motorcycle = new List<Motorcycles>();
-            
+            List<Motorcycles> motorcycles = new List<Motorcycles>();
+            Motorcycles honda = new Motorcycles();
+            motorcycles.Add(honda);
+            string json = JsonSerializer.Serialize(honda);
+            honda.id = Guid.NewGuid();
+            honda.Name = "HONDA";
+            honda.Model = "CB500x";
+            honda.Year = 2020;
+            honda.Odometer = 0;
+            honda.CreateMotorcycle();
+            honda.GetMotorCycleByID();
+            honda.GetMotorcycles();
+            honda.UpdateMotorycle();
+            honda.DeleteMotorcycle();
+
         }
     }
 }
